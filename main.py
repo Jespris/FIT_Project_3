5ccdc657f4285b01d563b65db5f7ade879f65156
Jesper Andersson <87135834+Jespris@users.noreply.github.com>
2024-04-30 12:57:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from question_7 import question_7
 
 
 def main():
+    analyse_everything = True
+
+    # The dictionary below have values that are functions without ()
+    # These then get called later on, or if analyse_everything is set to false,
+    # then choose the specific question to analyse
+    # TODO: add all the questions starting function to this dictionary
     questions = {
         1: question_1,
         3: question_3,
         7: question_7
     }
 
-    # Which question do you want analysed?
-    questions[7]()
-
-
+    if not analyse_everything:
+        # Which specific question do you want analysed?
+        questions[7]()
+    else:
+        print("Analysing everything!")
+        for value in questions.values():
+            value()
 
 
 if __name__ == '__main__':
